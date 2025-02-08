040d90dfeb19887bc68ac5fab314983206ee155f
DESKTOP-95T5PHA\Javi <Javi>
2025-02-08 19:29:00 +0100

    feat: initial commit project

---
+++ src/main/java/com/lolytics/LolyticsApplication.java
@@ -0,0 +1,13 @@
+package com.lolytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lolytics/TestLolyticsApplication.java
@@ -0,0 +1,11 @@
+package com.lolytics;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestLolyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(LolyticsApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
